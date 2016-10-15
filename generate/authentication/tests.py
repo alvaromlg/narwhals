5a164a754c267bb43e073d1aad1977c1c3d6ed69
Alvaro <apelegrinafernandez@gmail.com>
2016-10-15 11:29:13 +0000

    Fixing tests for auth application.

--- generate/authentication/tests.py
+++ generate/authentication/tests.py
@@ -11,11 +11,14 @@ from django.conf import settings
 from models import User
 from views import UserList
 
+
 class CreateUserTest(APITestCase):
     def setup(self):
+        self.api_key = settings.API_KEY
         self.superuser = User.objects.create_superuser('vishnu@vishnu.com', '1989-09-26', 'vishnupassword')
         self.client.login(username='vishnu', password='vishnupassword')
         self.data = {'email': 'test@gmail.com',
+                     'id': 1,
                      'date_of_birth': '1989-09-26',
                      'position': 1,
                      'meters': 1,
@@ -30,13 +33,24 @@ class CreateUserTest(APITestCase):
                      'bio': None,
                      'password': '12346'
                      }
+        self.token = Token.objects.get(user_id=self.superuser.id)
 
     def test_can_create_user(self):
         self.setup()
-        self.token = Token.objects.get(user_id=self.superuser.id)
-        self.api_key = settings.API_KEY
-        self.factory = APIRequestFactory()
-        self.response = self.client.post('/api/v1/users/?api_key=%s' % self.api_key, 
+        response = self.client.post('/api/v1/users/?api_key=%s' % self.api_key,
                                     self.data, 
                                     HTTP_AUTHORIZATION='Token {}'.format(self.token))
-        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_can_check_session(self):
+        self.setup()
+        response = self.client.get('/api/v1/check-session/?api_key=%s' % self.api_key,
+                                  HTTP_AUTHORIZATION='Token {}'.format(self.token))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_get_users(self):
+        self.setup()
+        response = self.client.get('/api/v1/users/?api_key=%s' % self.api_key,
+                                   HTTP_AUTHORIZATION='Token {}'.format(self.token))
+        print response
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
